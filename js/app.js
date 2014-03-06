7f22121a435eb1142d3eb114d2210fd3d9775041
Emily Tran <tran.thn@gmail.com>
2014-03-05 21:35:45 -0500

    Added basic spine calculator, only takes in 3 vars now...

--- js/app.js
+++ js/app.js
@@ -1,7 +1,36 @@
 var app = angular.module('archeryApp', []);
 
 app.controller('formCtrl', function($scope) {
-    $scope.testFn = function() {
-        console.log("Hello!");
+    $scope.deflection = null;
+    
+    $scope.calculate = function() {
+        var pd = $scope.pound,
+            draw = $scope.draw,
+            pt = $scope.point,
+            ptDiff = pt - 125,
+            drawDiff = draw - 28,
+            divisor = pd,
+            deflection = 0;
+
+        // account for heavier field points
+        if (ptDiff > 0) {
+            var rem = ptDiff /  25;
+            divisor += (rem * 5);
+        }
+
+        // account for longer / shorter draws
+        // < 28' draws generated a negative drawDiff
+        if (drawDiff > 0) {
+            divisor += drawDiff * 5;
+        }
+
+        $scope.deflection = (26 / divisor).toFixed(3);
+    }
+
+    $scope.clear = function() {
+        $scope.deflection = null;
+        $scope.pound = '';
+        $scope.draw = '';
+        $scope.point = '';
     }
 });
